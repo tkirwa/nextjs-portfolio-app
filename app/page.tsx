98e9e522bd5ea016fdc828e4f49f4d385679c7e1
Tonny Kirwa <tonnykirwa@yahoo.com>
2025-09-11 11:47:43 +0300

    added card ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import { Contact } from "lucide-react";
+import Skills from "./sections/Skills";
 
 export default function Home() {
   return (
---
+++ components/ui/card.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export function Card({ children, className, ...props }: CardProps) {
+  return (
+    <div
+      className={cn(
+        "rounded-lg border bg-white p-6 shadow-sm transition hover:shadow-md",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
